c88d6f58a80555f30b352380f42bdaee1e7434e4
n.feofanov <n.feofanov@visionlabs.ru>
2019-02-28 22:39:23 +0300

    add disconnect on win close/refresh; add possible marks and message from server; add 'spectractor' for front

--- app/run.py
+++ app/run.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-import os
+from flask_socketio import SocketIO, emit, disconnect
 
-cnt = 0
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SECRET_KEY'] = 'secret!'    # dn wtf is it
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0     # try to disable cache in browser
 socketio = SocketIO(app)
 
+POSSIBLE_MARKS = ('0', '1', '2', '3', '5', '8', '9')
+
 pollResults = dict()
+
+
+def emitResults():
+    if len([name for name in pollResults if pollResults[name]['state'] is True]) == len(pollResults):
+        resp = [{'name': name, 'mark': pollResults[name]['mark']} for name in pollResults]
+    else:
+        resp = [{'name': name, 'state': pollResults[name]['state']} for name in pollResults]
+    emit('poll', resp, broadcast=True)
+
+
+def emitBadMark():
+    emit('auth_resp', 'Bad mark. Possible marks are: '+','.join(POSSIBLE_MARKS))
+    resp = [{'name': name, 'state': pollResults[name]['state']} for name in pollResults]
+    emit('poll', resp, broadcast=True)
 
 
 @app.route('/')
@@ -21,16 +35,26 @@ def auth(login):
     resp = {'txt': 'Hello, ', 'login': login.capitalize()}
     pollResults[login.capitalize()] = {'mark': False, 'state': False}
     emit('auth_resp', resp)
+    emitResults()
+
+
+@socketio.on('disc')
+def disc(login):
+    try:
+        pollResults.pop(str(login))
+    except:
+        pass
+    finally:
+        emitResults()
+        disconnect()
 
 
 @socketio.on('reset')
 def reset():
-    global pollResults
     for name in pollResults:
         pollResults[name]['state'] = False
         pollResults[name]['mark'] = False
-    states = [{name: pollResults[name]['state']} for name in pollResults]
-    emit('poll', states, broadcast=True)
+    emitResults()
 
 
 @socketio.on('poll')
@@ -40,13 +64,15 @@ def poll(data):
         emit('auth_resp', 'please, enter your name and reconnect')
     else:
         mark = data.get('mark')
-        pollResults[login] = {'state': True, 'mark': mark}
-
-        if len([name for name in pollResults if pollResults[name]['state'] is True]) == len(pollResults):
-            resp = [{name: pollResults[name]['mark']} for name in pollResults]
-        else:
-            resp = [{name: pollResults[name]['state']} for name in pollResults]
-        emit('poll', resp, broadcast=True)
+        if mark:
+            if mark not in POSSIBLE_MARKS:
+                pollResults[login]['state'] = False
+                pollResults[login].update({'mark': False})
+                emitBadMark()
+            else:
+                pollResults[login]['state'] = True
+                pollResults[login].update({'mark': mark})
+                emitResults()
 
 
 if __name__ == '__main__':
--- app/static/app.js
+++ app/static/app.js
@@ -1,50 +1,76 @@
-
-var socket = io.connect('http://' + document.domain + ':' + location.port + '/');
+let socket = io.connect('http://' + document.domain + ':' + location.port + '/');
 let myName = null;
 
 setTimeout(show_state, 200);
 
 function show_state() {
-  document.getElementById('socket_state').value = 'disconnected';
-  if (socket && socket.connected === true)
-    document.getElementById('socket_state').value = 'connected';
+    document.getElementById('socket_state').value = 'disconnected';
+    if (socket && socket.connected === true)
+        if (myName !== null) {
+            document.getElementById('socket_state').value = 'connected';
+        } else {
+            document.getElementById('socket_state').value = 'enter login and press connect';
+        }
 }
 
 function connect() {
-  if (!socket)
-    socket = io.connect('http://' + document.domain + ':' + location.port + '/');
-  let login = document.getElementById('login').value;
+    if (!socket)
+        socket = io.connect('http://' + document.domain + ':' + location.port + '/');
+    let login = document.getElementById('login').value;
 
-  if (login) {
-    socket.emit('auth', login);
-    setTimeout(show_state, 200);
-  } else {
-    document.getElementById('socket_state').value = 'disconnected';
-    document.getElementById('resp_auth').value = 'Please, enter login and press connect';
-  }
+    if (login) {
+        socket.emit('auth', login);
+        setTimeout(show_state, 200);
+    } else {
+        document.getElementById('socket_state').value = 'disconnected';
+        document.getElementById('resp_auth').value = 'Please, enter login and press connect';
+    }
 }
 
 socket.on('auth_resp', function (msg) {
-  myName = msg['login'];
-  document.getElementById('resp_auth').value = msg.txt + msg.login;
+    let name_ = msg['login'];
+    if (name_ !== undefined) {
+        myName = name_;
+        document.getElementById('resp_auth').value = msg.txt + msg.login;
+    } else {
+        document.getElementById('resp_auth').value = msg;
+    }
 });
 
 socket.on('poll', function (results) {
-  document.getElementById('serv_reply').value = JSON.stringify(results);
+    let output = '';
+    for (let res of results) {
+        output += res['name'];
+        if (res['state'] === true) {
+            output += ' ready';
+        } else if (res['state'] === false) {
+            output += ' think';
+        } else if (res['mark']) {
+            output += ': ' + res['mark'];
+        } else {
+            output += ' spectractor';
+        }
+        output += '\n';
+    }
+    document.getElementById('serv_reply').value = output;
 });
 
 function disconnect() {
-  if (socket) {
-    socket.disconnect();
-    socket = null;
-  }
-  setTimeout(show_state, 200);
+    if (socket) {
+        socket.emit('disc', myName);
+        socket = null;
+    }
+    setTimeout(show_state, 200);
+}
+
+window.onbeforeunload = function () {
+    disconnect();
 }
 
 function reset_poll() {
-  socket.emit('reset');
+    socket.emit('reset');
 }
 
 function vote() {
-  socket.emit('poll', {'login': myName, 'mark': document.getElementById('mark').value});
+    socket.emit('poll', {'login': myName, 'mark': document.getElementById('mark').value});
 }
